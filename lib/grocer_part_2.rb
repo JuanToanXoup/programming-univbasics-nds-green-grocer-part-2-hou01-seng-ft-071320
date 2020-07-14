2ff09eee395546b50355c2025d7c3d53676e2b2d
John Doan <johndoan42@gmail.com>
2020-07-14 03:38:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -87,7 +87,7 @@ def coupons_clearance(coupons)
 avocado = find_item_by_name_in_collection('AVOCADO', unconsolidated_cart)
 cart = [avocado, avocado]
 c = [coupons.first]
-expect(checkout(cart, c)).to eq(4.00)
+checkout(cart, c)
 end
 
 binding.pry
